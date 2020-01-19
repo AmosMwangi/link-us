f41eaac0e0f86413faee9f30851cdf845a4a2583
AmosMwangi <amosmwangi101@gmail.com>
2020-01-19 08:14:35 +0300

    included apps' url  and made some imports

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-# from django.urls import path
-from django.conf.urls import url
-from django.contrib.auth import views
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('',include('hood.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 
 ]
